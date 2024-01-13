8dee2f4f8e93e962d0c2d64b3bd3da406c01be10
Ayse Erdogan <aerdogan@AErdogan-Fyler.attlocal.net>
2024-01-13 01:23:44 -0600

    january

--- data-and-variables/exercises/exercises2.js
+++ data-and-variables/exercises/exercises2.js
@@ -1,9 +1,9 @@
 const input = require('readline-sync');
-
+console.log("Hello,");
 let name = input.question("Enter your name: ");
 console.log("Hello, " + name + "!")
 
 let num1 = input.question("Enter a number: ");
 let num2 = input.question("Enter another number: ");
 
-console.log(num1 + num2);
+console.log(parseInt(num1) + parseInt(num2));
