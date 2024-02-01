467d18a0878422c17d40e63691b19ef9817da1d0
Ayse Erdogan <aerdogan@AErdogan-Fyler.attlocal.net>
2024-02-01 17:02:17 -0600

    Feb-1

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -2,21 +2,99 @@ let superChimpOne = {
    name: "Chad",
    species: "Chimpanzee",
    mass: 9,
-   age: 6
+   age: 6,
+   astronautID: 1,
+   move: function () {return Math.floor(Math.random()*11)},
+   info: function(){console.log(`${this.name} is a ${this.species}. They are ${this.age} years old and ${this.mass} kilograms. Their ID is ${this.astronautID}.`)}
+
 };
 
 let salamander = {
    name: "Lacey",
    species: "Axolotl Salamander",
    mass: 0.1,
-   age: 5
+   age: 5,
+   astronautID: 2,
+   move: function () {return Math.floor(Math.random()*11)},
+   info: function(){console.log(`${this.name} is a ${this.species}. They are ${this.age} years old and ${this.mass} kilograms. Their ID is ${this.astronautID}.`)}
+
 };
 
+let superChimpTwo = {
+   name: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 6,
+   astronautID: 3,
+   move: function () {return Math.floor(Math.random()*11)},
+   info: function(){console.log(`${this.name} is a ${this.species}. They are ${this.age} years old and ${this.mass} kilograms. Their ID is ${this.astronautID}.`)}
 
-// After you have created the other object literals, add the astronautID property to each one.
+};
 
-// Create an array to hold the animal objects.
+let dog = {
+   name: "Leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 5,
+   astronautID: 4,
+   move: function () {return Math.floor(Math.random()*11)},
+   info: function(){console.log(`${this.name} is a ${this.species}. They are ${this.age} years old and ${this.mass} kilograms. Their ID is ${this.astronautID}.`)}
 
-// Print out the relevant information about each animal.
+};
+
+let waterBear = {
+   name: "Almina",
+   species: "Tardigrade",
+   mass: 0.0000000001,
+   age: 1,
+   astronautID: 5,
+   move: function () {return Math.floor(Math.random()*11)},
+   info: function(){console.log(`${this.name} is a ${this.species}. They are ${this.age} years old and ${this.mass} kilograms. Their ID is ${this.astronautID}.`)}
+};
+
+let crew = [superChimpOne, superChimpTwo, salamander, dog, waterBear]; 
+for (let i=0; i<crew.length; i++){
+   crew[i].info();
+}
+
+// superChimpOne.info();
+// salamander.info();
+// superChimpTwo.info();
+// dog.info();
+// waterBear.info();
+
+
+
+//// After you have created the other object literals, add the astronautID property to each one.
+
+//// Create an array to hold the animal objects.
+
+//// Print out the relevant information about each animal.
 
 // Start an animal race!
+
+function fitnessTest(candidates){
+   let results = [], numSteps, turns;
+   candidates=crew;
+   for (let i = 0; i < candidates.length; i++){
+       numSteps = 0;
+       turns = 0;
+       while(numSteps < 20){
+       numSteps += candidates[i].move();
+       turns++;
+       }
+       results.push(`${candidates[i].name} took ${turns} turns to take 20 steps.`);
+   }
+   return results;
+  }
+//   console.log(fitnessTest(superChimpOne));
+//   console.log(fitnessTest(superChimpTwo));
+//   console.log(fitnessTest(salamander));
+//   console.log(fitnessTest(dog));
+//   console.log(fitnessTest(waterBear));
+
+// for(let i=0; i<crew.length; i++){
+//  console.log(fitnessTest(crew[i]));
+// }  
+console.log(fitnessTest(crew));
+console.log('-------------')
---
+++ zexercises/ex-Object-Math.js
@@ -0,0 +1,118 @@
+
+let giraffe = {
+    species: "Reticulated Giraffe",
+    name: "Cynthia",
+    weight: 1500,
+    age: 15,
+    diet: "leaves",
+    birthday: function () {
+        giraffe.age= giraffe.age+1;}
+  };
+ 
+//   function birthday(animal) {
+//       animal.age = animal.age + 1;
+//       return animal;
+//   }
+ 
+  console.log(giraffe["age"]);
+ 
+  giraffe.birthday();
+ 
+  console.log(giraffe.age);
+
+
+
+//   let tortoiseOne = {
+//     age: 150,
+//     species: "Galapagos Tortoise",
+//     diet: ["pumpkins", "lettuce", "cabbage"]
+//  };
+
+//  let tortoiseTwo = {
+//     age: 150,
+//     species: "Galapagos Tortoise",
+//     diet: ["pumpkins", "lettuce", "cabbage"]
+//  };
+
+//  if (tortoiseOne == tortoiseTwo){
+//     console.log("tortoiseOne == tortoiseTwo true")
+//  }else{
+//     console.log("tortoiseOne == tortoiseTwo false")
+//  };
+
+//  if (tortoiseOne === tortoiseTwo){
+//     console.log("tortoiseOne === tortoiseTwo true")
+//  }else{
+//     console.log("tortoiseOne === tortoiseTwo false")
+//  }
+
+//  if (tortoiseOne.age == tortoiseTwo.age){
+//     console.log("tortoiseOne.age == tortoiseTwo.age true")
+//  }else{
+//     console.log("tortoiseOne.age == tortoiseTwo.age false")
+//  }
+
+
+// let num = Math.floor(Math.random()*10);
+//     console.log(num);
+
+// let num = Math.round(Math.random()*10);
+
+//    console.log(num);
+
+// let num1 =Math.floor(-3.87);
+// console.log(num1);//-4 down
+
+// let num2 =Math.random(-3.87);
+// console.log(num2);// random number
+
+// let num3 =Math.round(-3.87);
+// console.log(num3);//-4 nearest
+   
+// let num4 =Math.trunc(-3.87);
+// console.log(num4);//-3
+
+// let num5=Math.abs(-7);
+// console.log(num5);//7
+
+// let num6=Math.ceil(3.14);
+// console.log(num6);//4 up to
+
+// let num7=Math.max(2,4,1,6,9,110);
+// console.log(num7);//110
+
+// let num8=Math.min(2,4,1,6,9,110);
+// console.log(num8);//1
+
+// let num9=Math.pow(5,3); 
+// console.log(num9);//125
+
+// let num10=Math.sqrt(36);
+// console.log(num10);//36
+
+// 
+console.log(Math.round(12.3456789)*10000/10000);
+
+console.log(Math.round(12.3456789*100)/100);
+
+console.log(Math.round(12.3456789)*100/100);
+
+console.log(Math.round(12.3456789*10000)/10000);
+
+
+
+
+// function randomSelection(arr){
+//     let index = Math.floor(Math.random()*arr.length);
+//     return arr[index];
+//  }
+
+//  let happiness = ['Hope','Joy','Peace','Love','Kindness','Puppies','Kittens','Tortoise'];
+
+//  for (i=0; i < 8; i++){
+//     console.log(randomSelection(happiness));
+//  }
+
+
+
+
