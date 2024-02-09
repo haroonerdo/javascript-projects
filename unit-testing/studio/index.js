08e987d422cd9b158e49f97cc92fcda5a9fc0337
Ayse Erdogan <aerdogan@AErdogan-Fyler.attlocal.net>
2024-02-08 20:21:30 -0600

    Feb-8 unit-testing studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,41 @@
 
 let launchcode = {
-  
-}
+  organization:"nonprofit",
+  executiveDirector:"Jeff",
+  percentageCoolEmployees:100,
+  programsOffered:["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput : (num)=>{
+    let output="";
+    if (num%2 ===0 && num%3 !==0 && num%5 !==0){
+        output = "Launch"
+    }
+    else if (num%2 !==0 && num%3 ===0 && num%5 !==0){
+        output = "Code"
+    }
+    else if (num%2 !==0 && num%3 !==0 && num%5 ===0){
+        output = "Rocks"
+    }
+    else if (num%2 ===0 && num%3 ===0 && num%5 !==0){
+        output = "LaunchCode"
+    }
+    else if (num%2 !==0 && num%3 ===0 && num%5 ===0){
+        output = "Code Rocks"
+    }
+    else if (num%2 ===0 && num%3 !==0 && num%5 ===0){
+        output = "Launch Rocks"
+    }
+    else if (num%2 ===0 && num%3 ===0 && num%5 ===0){
+        output = "LaunchCode Rocks"
+    }
+    else if (num%2 ===0 && num%3 ===0 && num%5 !==0){
+        output = "LaunchCode"
+    }
+    else {
+output="Rutabagas! That doesn't work."
+    }
+    return output;
+  }
 
+}
 module.exports = launchcode;
 
