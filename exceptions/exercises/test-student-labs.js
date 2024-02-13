f0cf3e88438b41a1cbbe1acb5da210c138d1322b
Ayse Erdogan <aerdogan@AErdogan-Fyler.attlocal.net>
2024-02-12 20:05:23 -0600

    Feb-12

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -45,4 +45,27 @@ let studentLabs = [
   }
 ];
 
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+
 gradeLabs(studentLabs);
+gradeLabs(studentLabs2);
